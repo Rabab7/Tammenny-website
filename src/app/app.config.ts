669b031ec14f37e425a02857dd2e0a12bdbab98e
Rabab7 <rababalielshelh@gmail.com>
2025-12-10 02:44:29 +0200

    commit3

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,20 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import {environment}  from './core/environments/environment';
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ]
 };
---
+++ src/app/core/environments/environment.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDaa6R1NflP9NVWOklmqdyTCIFKn_Hpp0o',
+    authDomain: 'tammenny-370da.firebaseapp.com',
+    projectId: 'tammenny-370da',
+    storageBucket: 'tammenny-370da.firebasestorage.app',
+    messagingSenderId: '348362479368',
+    appId: '1:348362479368:web:bdd1a87c337f6f8aa07ebd',
+    measurementId: 'G-347WW319WG',
+  },
+};
